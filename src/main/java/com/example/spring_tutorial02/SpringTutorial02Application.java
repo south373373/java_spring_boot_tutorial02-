a240b7fde86409297a9608d988129b25dfee9d89
t-minami <minamiwave@gmail.com>
2025-05-24 20:24:37 +0900

    #01_commit_sam06

---
+++ src/main/java/com/example/spring_tutorial02/SpringTutorial02Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_tutorial02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorial02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorial02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_tutorial02/SpringTutorial02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_tutorial02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorial02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
