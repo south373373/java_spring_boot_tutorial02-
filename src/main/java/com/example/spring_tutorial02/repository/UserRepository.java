79e13e1a94414d25221559153c5a084a1b5c51dd
t-minami <minamiwave@gmail.com>
2025-05-25 10:11:36 +0900

    #04_commit_sam11

--- src/main/java/com/example/spring_tutorial02/repository/UserRepository.java
+++ src/main/java/com/example/spring_tutorial02/repository/UserRepository.java
@@ -1,3 +1,11 @@
-public class UserRepository {
-    
+package com.example.spring_tutorial02.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.springtutorial.entity.User;
+
+// 「JpaRepository<User, Integer>」にてリポジトリとして扱いたい、対象Entity名と主キーを指定。
+public interface UserRepository extends JpaRepository<User, Integer>{
+    // ユーザー名で完全一致検索
+    List<User> findByUserName(String userName);
 }
