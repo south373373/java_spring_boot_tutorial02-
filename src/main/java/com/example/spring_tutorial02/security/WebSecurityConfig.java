35e9a68394ec805c6c3044037deb6e729fb1a228
t-minami <minamiwave@gmail.com>
2025-05-31 01:42:38 +0900

    #09_commit_sam17

---
+++ src/main/java/com/example/spring_tutorial02/security/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.spring_tutorial02.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((requests) -> requests
+                .requestsMatchers("/login", "/resources/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin((form) -> form
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/adminuser?loggedIn")
+                .failureUrl("/login?/error")
+                .permitAll()
+            )
+            .loggout((logout) -> logout
+                .loggoutSuccessUrl("/?loggedOut")
+                .permitAll()
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
